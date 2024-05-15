d6219f4df02f14b30f75573dd1bbd81260b15250
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2024-05-15 18:35:17 +0000

    Fix the backend script logic

--- name_generator/build.py
+++ name_generator/build.py
@@ -1,21 +1,26 @@
 from PIL import Image, ImageDraw, ImageFont
-import os
+import os, shutil
 
 font_path = "name_generator/Font.ttf"
 
-def generate_certificate(certificate_template, names_list):
+def generate_certificate(certificate_template, names_file, directory):
     font = ImageFont.truetype(font_path, size=84)
 
-    x_position = 400
-    y_position = 400
+    with Image.open(certificate_template) as img:
+        width, height = img.size
 
     if not os.path.exists('Certificates'):
         os.makedirs('Certificates')
 
+    names_list = open(names_file, 'r')
+
     for name in names_list:
         template_image = Image.open(certificate_template)
         draw = ImageDraw.Draw(template_image)
-        draw.text((x_position, y_position), name, font=font, fill="black")
+        draw.text((width, height), name, font=font, fill="black")
         template_image.save(f"Certificates/certificate_{name}.jpg")
 
     names_list.close()
+
+    shutil.make_archive('Certificates', 'zip', 'Certificates')
+    shutil.move('Certificates.zip', directory)
--- static/script.js
+++
@@ -1,5 +0,0 @@
-function showFileName(event, outputElementId) {
-    var input = event.srcElement;
-    var fileName = input.files[0].name;
-    document.getElementById(outputElementId).textContent = "File selected: " + fileName;
-}
