bde55e1817996cf26fec132b6842c1d9c25a12fc
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2024-05-15 19:25:59 +0000

    created backend

--- gen.py
+++ gen.py
@@ -1,15 +1,15 @@
-from flask import Flask, render_template, json, request
+from flask import Flask, render_template, request
 from name_generator.build import generate_certificate
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def frontpage():
-    if request.method == 'POST':   
-        cert_image = request.files['file-upload']
-        names = request.files['file-upload-2']
-
-        generate_certificate(cert_image, names)
+    if request.method == 'POST':
+        cert_image = request.files['cert_image']
+        names = request.files['names']
+        directory = request.form['directory']
+        generate_certificate(cert_image, names, directory)
 
     return render_template('home.html')
 
