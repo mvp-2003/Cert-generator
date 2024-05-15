301e1bd9cab7b6f25f6d14df6ae6656c27558dbe
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2024-05-15 15:30:08 +0000

    Connecting backend

--- gen.py
+++ gen.py
@@ -1,9 +1,16 @@
-from flask import Flask, render_template, json
+from flask import Flask, render_template, json, request
+from name_generator.build import generate_certificate
 
 app = Flask(__name__)
 
 @app.route('/')
 def frontpage():
+    if request.method == 'POST':   
+        cert_image = request.files['file-upload']
+        names = request.files['file-upload-2']
+
+        generate_certificate(cert_image, names)
+
     return render_template('home.html')
 
 app.run(port=5000)
--- name_generator/build.py
+++ name_generator/build.py
@@ -1,23 +1,21 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
-certificate_template = "Certificate.png"
-font_path = "Font.ttf"
+font_path = "name_generator/Font.ttf"
 
-font = ImageFont.truetype(font_path, size=84)
+def generate_certificate(certificate_template, names_list):
+    font = ImageFont.truetype(font_path, size=84)
 
-names_list = open("names.txt", "r")
+    x_position = 400
+    y_position = 400
 
-x_position = 400
-y_position = 400
+    if not os.path.exists('Certificates'):
+        os.makedirs('Certificates')
 
-if not os.path.exists('Certificates'):
-    os.makedirs('Certificates')
+    for name in names_list:
+        template_image = Image.open(certificate_template)
+        draw = ImageDraw.Draw(template_image)
+        draw.text((x_position, y_position), name, font=font, fill="black")
+        template_image.save(f"Certificates/certificate_{name}.jpg")
 
-for name in names_list:
-    template_image = Image.open(certificate_template)
-    draw = ImageDraw.Draw(template_image)
-    draw.text((x_position, y_position), name, font=font, fill="black")
-    template_image.save(f"Certificates/certificate_{name}.jpg")
-
-names_list.close()
+    names_list.close()
