7380290edd59f71829d58e26fb62bbf5c7d2b84f
Rishabh <113057571+mvp-2003@users.noreply.github.com>
2024-05-19 16:24:32 +0000

    fix

--- gen.py
+++ gen.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from name_generator.build import generate_certificate
+import os
 
 app = Flask(__name__)
 
@@ -9,8 +10,12 @@ def frontpage():
         cert_image = request.files['cert_image']
         names = request.files['names']
         directory = request.form['directory']
-        generate_certificate(cert_image, names, directory)
+
+        cert_image_path = os.path.join('uploads', cert_image.filename)
+        names_path = os.path.join('uploads', names.filename)
+        cert_image.save(cert_image_path)
+        names.save(names_path)
+
+        generate_certificate(cert_image_path, names_path, directory)
 
     return render_template('home.html')
-
-app.run(port=5000)
--- name_generator/build.py
+++ name_generator/build.py
@@ -1,9 +1,9 @@
+import os
+import shutil
 from PIL import Image, ImageDraw, ImageFont
-import os, shutil
-
-font_path = "name_generator/Font.ttf"
 
 def generate_certificate(certificate_template, names_file, directory):
+    font_path = "arial.ttf"
     font = ImageFont.truetype(font_path, size=84)
 
     with Image.open(certificate_template) as img:
@@ -19,7 +19,11 @@ def generate_certificate(certificate_template, names_file, directory):
             draw = ImageDraw.Draw(template_image)
             text_width, text_height = draw.textsize(name, font=font)
             draw.text(((width - text_width) / 2, (height - text_height) / 2), name, font=font, fill="black")
-            template_image.save(f"Certificates/certificate_{name}.jpg")
+            template_image.save(f"Certificates/certificate_{name}.{certificate_template.split('.')[-1]}")
 
     shutil.make_archive('Certificates', 'zip', 'Certificates')
+
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+
     shutil.move('Certificates.zip', directory)
